8b94d0893b03f311f1343ad85bb404ce4f9b7beb
Neil Macneale <neil.macneale.v@gmail.com>
2019-11-24 18:47:10 -0800

    Able to render canvas with new WX library

--- canvas_manager.py
+++ canvas_manager.py
@@ -1,47 +1,92 @@
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk as gtk
-from gi.repository import Gdk as gdk
+import wx
 import canvas as canvas_module
 import toolbar as toolbar_module
 import tileset as tileset_module
 import tool_settings as tool_settings_module
 
 class CanvasManager:
-  def __init__(self, window, toolbar):
-    self.window = window
-    self.toolbar = toolbar
-    self.tool_settings = tool_settings_module.create(window)
-    self.canvases = []
-    self.current_canvas = -1
-    self.da = gtk.DrawingArea()
-    self.da.set_size_request(960, 540) # 0.5 * 1080p
-    self.da.set_hexpand(True)
-    self.da.set_vexpand(True)
-    self.da.connect("draw", self.draw)
-    window.connect("key-press-event", self.key_press)
-    window.connect("key-release-event", self.key_release)
-    self.event_box = gtk.EventBox()
-    self.event_box.connect("button-press-event", self.click)
-    self.event_box.connect("button-release-event", self.release)
-    self.event_box.connect("motion-notify-event", self.move)
-    self.event_box.connect("scroll-event", self.scroll)
-    self.event_box.add_events(
-        gdk.EventMask.BUTTON_PRESS_MASK
-      | gdk.EventMask.BUTTON_RELEASE_MASK
-      | gdk.EventMask.POINTER_MOTION_MASK
-      | gdk.EventMask.SCROLL_MASK)
-    self.event_box.add(self.da)
+  def __init__(self, pnl, window, toolbar):
+    buttons = [
+            wx.Button(pnl, label="ONE"),
+            wx.Button(pnl, label="TWO"),
+            wx.Button(pnl, label="TREE")]
 
-    self.tab_switcher = gtk.Box(orientation=gtk.Orientation.HORIZONTAL)
-    self.tab_switcher.show()
+    self.pnl = wx.Panel(pnl)
+    self.pnl.Bind(wx.EVT_PAINT, self.onPaint)
+    self.pnl.SetBackgroundColour("#E6E6E6")
 
-    self.box = gtk.Box(orientation=gtk.Orientation.VERTICAL)
-    self.box.pack_start(self.tab_switcher, False, False, 0)
-    self.box.pack_start(self.event_box, True, True, 0)
-    self.box.show()
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    for btn in buttons:
+      sizer.Add(btn, 0, wx.ALL, 5)
+    sizer.Add(self.pnl, 1, wx.EXPAND|wx.ALL, 5)
+    pnl.SetSizer(sizer)
 
-    self.new(None)
+    # self.add(self.grid)
+
+    # self.window = window
+    # self.toolbar = toolbar
+    # self.tool_settings = tool_settings_module.create(window)
+    # self.canvases = []
+    # self.current_canvas = -1
+    # self.da = gtk.DrawingArea()
+    # self.da.set_size_request(960, 540) # 0.5 * 1080p
+    # self.da.set_hexpand(True)
+    # self.da.set_vexpand(True)
+    # self.da.connect("draw", self.draw)
+    # window.connect("key-press-event", self.key_press)
+    # window.connect("key-release-event", self.key_release)
+    # self.event_box = gtk.EventBox()
+    # self.event_box.connect("button-press-event", self.click)
+    # self.event_box.connect("button-release-event", self.release)
+    # self.event_box.connect("motion-notify-event", self.move)
+    # self.event_box.connect("scroll-event", self.scroll)
+    # self.event_box.add_events(
+    #     gdk.EventMask.BUTTON_PRESS_MASK
+    #   | gdk.EventMask.BUTTON_RELEASE_MASK
+    #   | gdk.EventMask.POINTER_MOTION_MASK
+    #   | gdk.EventMask.SCROLL_MASK)
+    # self.event_box.add(self.da)
+
+    # self.tab_switcher = gtk.Box(orientation=gtk.Orientation.HORIZONTAL)
+    # self.tab_switcher.show()
+
+    # self.box = gtk.Box(orientation=gtk.Orientation.VERTICAL)
+    # self.box.pack_start(self.tab_switcher, False, False, 0)
+    # self.box.pack_start(self.event_box, True, True, 0)
+    # self.box.show()
+
+    # self.new(None)
+
+  def onPaint(self, event):
+    dc = wx.PaintDC(self.pnl)
+    dc.SetPen(wx.Pen('#d4d4d4'))
+
+    dc.SetBrush(wx.Brush('#c56c00'))
+    dc.DrawRectangle(10, 15, 90, 60)
+
+    dc.SetBrush(wx.Brush('#1ac500'))
+    dc.DrawRectangle(130, 15, 90, 60)
+
+    dc.SetBrush(wx.Brush('#539e47'))
+    dc.DrawRectangle(250, 15, 90, 60)
+
+    dc.SetBrush(wx.Brush('#004fc5'))
+    dc.DrawRectangle(10, 105, 90, 60)
+
+    dc.SetBrush(wx.Brush('#c50024'))
+    dc.DrawRectangle(130, 105, 90, 60)
+
+    dc.SetBrush(wx.Brush('#9e4757'))
+    dc.DrawRectangle(250, 105, 90, 60)
+
+    dc.SetBrush(wx.Brush('#5f3b00'))
+    dc.DrawRectangle(10, 195, 90, 60)
+
+    dc.SetBrush(wx.Brush('#4c4c4c'))
+    dc.DrawRectangle(130, 195, 90, 60)
+
+    dc.SetBrush(wx.Brush('#785f36'))
+    dc.DrawRectangle(250, 195, 90, 60)
 
   def get_current_canvas(self):
     return self.canvases[self.current_canvas]
@@ -106,5 +151,5 @@ class CanvasManager:
     canvas = self.canvases[self.current_canvas]
     self.window.update_tileset(canvas.tileset)
 
-def create(window, toolbar):
-  return CanvasManager(window, toolbar)
+def create(pnl, window, toolbar):
+  return CanvasManager(pnl, window, toolbar)
--- main.py
+++ main.py
@@ -1,39 +1,31 @@
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk as gtk
-from gi.repository import Gdk as gdk
+import wx
+import wx.grid as grid
 import menu_bar as menu_bar_module
 import canvas_manager as canvas_manager_module
 import toolbar as toolbar_module
 import tool_settings as tool_settings_module
 import tileset as tileset_module
 
-class MyWindow(gtk.Window):
+class TilemapFrame(wx.Frame):
   def __init__(self):
-    gtk.Window.__init__(self)
-    self.connect("destroy", gtk.main_quit)
-    self.set_default_size(300, 250)
-    settings = gtk.Settings.get_default()
-    settings.set_property("gtk-application-prefer-dark-theme", True)
+    super(TilemapFrame, self).__init__(None, title='Tilemap Editor')
 
-    header = gtk.HeaderBar(title="Tile Editor")
-    header.props.show_close_button = True
-    self.set_titlebar(header)
+    pnl = wx.Panel(self)
+    # tileset = tileset_module.create(pnl, 16, 16)
+    # tool_settings = tool_settings_module.create(pnl, self)
+    # toolbar = toolbar_module.create(pnl, self, tileset, tool_settings)
+    toolbar = None
+    canvas_manager = canvas_manager_module.create(pnl, self, toolbar)
+    # menu_bar = menu_bar_module.create(pnl, self, canvas_manager)
 
-    self.grid = gtk.Grid()
-    self.add(self.grid)
+    # self.grid.attach(menu_bar.widget(), 0, 0, 5, 1)
+    # self.grid.attach(toolbar.widget(), 1, 1, 1, 1)
+    # self.grid.attach(tool_settings.widget(), 1, 2, 1, 2)
+    # self.grid.attach(canvas_manager.widget(), 2, 1, 2, 3)
+    # self.grid.attach(gtk.Button(label="Click Here"), 4, 2, 1, 2)
 
-    tileset = tileset_module.create(16, 16)
-    tool_settings = tool_settings_module.create(self)
-    toolbar = toolbar_module.create(self, tileset, tool_settings)
-    canvas_manager = canvas_manager_module.create(self, toolbar)
-    menu_bar = menu_bar_module.create(self, canvas_manager)
-
-    self.grid.attach(menu_bar.widget(), 0, 0, 5, 1)
-    self.grid.attach(toolbar.widget(), 1, 1, 1, 1)
-    self.grid.attach(tool_settings.widget(), 1, 2, 1, 2)
-    self.grid.attach(canvas_manager.widget(), 2, 1, 2, 3)
-    self.grid.attach(gtk.Button(label="Click Here"), 4, 2, 1, 2)
+  def OnExit(self, event):
+    self.Close(True)
 
   def update_tileset(self, tileset):
     old_tileset = None
@@ -47,6 +39,10 @@ class MyWindow(gtk.Window):
     self.grid.attach(tileset.widget(), 4, 1, 1, 1)
     self.show_all()
 
-win = MyWindow()
-win.show_all()
-gtk.main()
+if __name__ == '__main__':
+  # When this module is run (not imported) then create the app, the
+  # frame, show it, and start the event loop.
+  app = wx.App()
+  frm = TilemapFrame()
+  frm.Show()
+  app.MainLoop()

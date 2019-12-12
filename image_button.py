839c8b5643b8ce2ce316023c1ccbba520cc94e1e
Neil Macneale <neil.macneale.v@gmail.com>
2019-12-12 11:05:01 -0800

    Removed gi imports

--- image_button.py
+++ image_button.py
@@ -1,6 +1,6 @@
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk as gtk
+# import gi
+# gi.require_version('Gtk', '3.0')
+# from gi.repository import Gtk as gtk
 import cairo
 
 class Button:
